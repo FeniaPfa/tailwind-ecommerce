2e1052544c7895de3aa972b3ed8214e01db7c1f7
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-05-04 21:15:00 -0400

    feat: fetchApi, add: redux toolkit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import { Card } from '../components/Card';
 import { Layout } from '../components/Layout';
+import { useEffect } from 'react';
 
 export const Home = () => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setItems(data);
+            });
+    }, []);
+
     return (
         <Layout>
-            <Card />
+            <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+                {items?.map((item) => (
+                    <Card key={item.id} data={item} />
+                ))}
+            </div>
         </Layout>
     );
 };
