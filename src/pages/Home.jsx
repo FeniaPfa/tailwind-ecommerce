245c8486ca90a6f67ee7b9278313356f1e7c81a3
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-05-31 17:23:27 -0400

    feat: filterProducts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Card } from '../components/Card';
 import { Layout } from '../components/Layout';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ProductDetail } from '../components/ProductDetail';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProducts } from '../redux/productsSlice';
@@ -9,16 +9,41 @@ export const Home = () => {
     const { isOpen } = useSelector((state) => state.productDetail);
     const dispatch = useDispatch();
     const { products, loading } = useSelector((state) => state.products);
+    const [filteredItems, setFilteredItems] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+
+    const handleSearch = (e) => {
+        setSearchValue(e.target.value);
+    };
 
     useEffect(() => {
         dispatch(fetchProducts());
     }, [dispatch]);
 
+    useEffect(() => {
+        let newData = [...products];
+        newData = newData.filter((item) =>
+            item.title.toLowerCase().includes(searchValue.toLowerCase())
+        );
+
+        setFilteredItems(newData);
+    }, [searchValue]);
+
     return (
         <Layout>
             {loading && <p>Cargando...</p>}
+            <div className="flex w-80 items-center justify-center relative mb-4">
+                <h1 className="font-medium text-xl">Exclusive Products</h1>
+            </div>
+            <input
+                className="rounded-lg border border-black w-80 p-4 mb-4"
+                type="text"
+                placeholder="Find products"
+                onChange={handleSearch}
+            />
+            {filteredItems.length === 0 && <p>No results</p>}
             <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
-                {products?.map((item) => (
+                {filteredItems?.map((item) => (
                     <Card key={item.id} data={item} />
                 ))}
             </div>
