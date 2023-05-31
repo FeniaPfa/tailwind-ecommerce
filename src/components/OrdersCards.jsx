0c5ba735b5d2edb64730a0538c7c9315d44b0f95
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-05-31 13:22:18 -0400

    style: :lipstick: add OrdersCard icons

--- src/components/OrdersCards.jsx
+++ src/components/OrdersCards.jsx
@@ -1,13 +1,23 @@
-import { ChevronRightIcon } from '@heroicons/react/24/outline';
+import {
+    ChevronRightIcon,
+    CalendarIcon,
+    ShoppingBagIcon,
+} from '@heroicons/react/24/outline';
 
 export const OrdersCards = ({ totalQuantity, totalPrice, date }) => {
     return (
         <div className="flex mb-4 justify-between items-center border border-black w-80 p-4 rounded-lg">
             <div className="flex justify-between w-full">
-                <p className="flex flex-col">
-                    <span className="font-light">{date}</span>
-                    <span className="font-light">{totalQuantity} articles</span>
-                </p>
+                <div className="flex flex-col gap-2">
+                    <p className="flex gap-2">
+                        <CalendarIcon className="text-black h-6 w-6" />
+                        <span className="font-light">{date}</span>
+                    </p>
+                    <p className="flex gap-2">
+                        <ShoppingBagIcon className="text-black h-6 w-6" />
+                        <span className="font-light">{totalQuantity} articles</span>
+                    </p>
+                </div>
                 <p className="flex gap-2 items-center">
                     <span className="font-medium text-2xl">$ {totalPrice}</span>
                     <ChevronRightIcon className="h-6 w-6 text-black cursor-pointer" />
