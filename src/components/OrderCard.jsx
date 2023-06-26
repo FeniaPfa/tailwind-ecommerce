6005b1fbb2d5d8e6fe0092bdf2521ba8955f4fd3
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-06-26 16:43:25 -0400

    fix: OrdersCard

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -1,4 +1,4 @@
-import { XMarkIcon, MinusCircleIcon, PlusCircleIcon } from '@heroicons/react/24/outline';
+import { TrashIcon, MinusCircleIcon, PlusCircleIcon } from '@heroicons/react/24/outline';
 import { useDispatch } from 'react-redux';
 import { addOne, deleteProduct, removeOne } from '../redux/cartSlice';
 import { formatPrice } from '../utils';
@@ -19,26 +19,34 @@ export const OrderCard = ({ product, myOrder }) => {
         <div className="flex justify-between items-center">
             <div className="flex items-center gap-2">
                 <figure className="w-20 h-20">
-                    <img className="w-full h-full rounded-lg object-cover" src={product.images[0]} alt={product.title} />
+                    <img className="aspect-square rounded-lg object-cover" src={product.images[0]} alt={product.title} />
                 </figure>
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-1">
                     <p className="text-sm">{product.title}</p>
-                    <p className="text-sm font-light">Quantity: {product.quantity}</p>
-                    <div className="flex gap-2">
-                        <button>
-                            <MinusCircleIcon className="w-5 cursor-pointer" onClick={handleRemove} />
-                        </button>
-                        <span>{product.quantity}</span>
-                        <button>
-                            <PlusCircleIcon className="w-5 cursor-pointer" onClick={handleAdd} />
-                        </button>
-                    </div>
-                    <p className="text-sm font-light">${formatPrice(product.price)}</p>
+
+                    <p className="text-sm font-light">Quantity:</p>
+                    {!myOrder && (
+                        <div className="flex gap-2">
+                            <button>
+                                <MinusCircleIcon className="w-5 cursor-pointer text-blue-500" onClick={handleRemove} />
+                            </button>
+                            <span>{product.quantity}</span>
+                            <button>
+                                <PlusCircleIcon className="w-5 cursor-pointer text-blue-500" onClick={handleAdd} />
+                            </button>
+                        </div>
+                    )}
                 </div>
             </div>
-            <div className="flex items-center gap-2">
-                <p className="text-lg font-medium">${formatPrice(product.price * product.quantity)}</p>
-                {!myOrder && <XMarkIcon onClick={handleDelete} className="h-5 w-5 text-blue-500 cursor-pointer" />}
+            <div>
+                <p className="text-sm font-light">
+                    Price: <span className="font-normal">${formatPrice(product.price)}</span>
+                </p>
+                <p className="text-sm font-light">Unitary Total:</p>
+                <div className="flex items-center gap-2">
+                    <p className="text-lg font-medium">${formatPrice(product.price * product.quantity)}</p>
+                    {!myOrder && <TrashIcon onClick={handleDelete} className="h-5 w-5 text-blue-500 cursor-pointer" />}
+                </div>
             </div>
         </div>
     );
