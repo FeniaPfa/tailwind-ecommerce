90e93071e817df0adeaf71bdadb69e91a4242d5b
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-06-25 21:59:24 -0400

    feat(cart): removeOne, addOne

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -1,12 +1,18 @@
-import { XMarkIcon } from '@heroicons/react/24/outline';
+import { XMarkIcon, MinusCircleIcon, PlusCircleIcon } from '@heroicons/react/24/outline';
 import { useDispatch } from 'react-redux';
-import { deleteProduct } from '../redux/cartSlice';
+import { addOne, deleteProduct, removeOne } from '../redux/cartSlice';
 import { formatPrice } from '../utils';
 
 export const OrderCard = ({ product, myOrder }) => {
     const dispatch = useDispatch();
     const handleDelete = () => {
         dispatch(deleteProduct(product));
+    };
+    const handleAdd = () => {
+        dispatch(addOne(product.id));
+    };
+    const handleRemove = () => {
+        dispatch(removeOne(product.id));
     };
 
     return (
@@ -18,7 +24,16 @@ export const OrderCard = ({ product, myOrder }) => {
                 <div className="flex flex-col gap-2">
                     <p className="text-sm">{product.title}</p>
                     <p className="text-sm font-light">Quantity: {product.quantity}</p>
-                    <p className="text-sm font-light">${product.price}</p>
+                    <div className="flex gap-2">
+                        <button>
+                            <MinusCircleIcon className="w-5 cursor-pointer" onClick={handleRemove} />
+                        </button>
+                        <span>{product.quantity}</span>
+                        <button>
+                            <PlusCircleIcon className="w-5 cursor-pointer" onClick={handleAdd} />
+                        </button>
+                    </div>
+                    <p className="text-sm font-light">${formatPrice(product.price)}</p>
                 </div>
             </div>
             <div className="flex items-center gap-2">
